82d1811e1f76b414a6dc7809c9d3b1304ad33379
Byte11 <shirmak11@gmail.com>
2018-12-15 09:13:12 -0600

    Got rid of servo control MainTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -62,31 +62,12 @@ public class MainTeleOp extends OpMode {
      * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
      */
 
-    int i = 0;
-
     @Override
     public void loop() {
-        i++;
 
         //Driving
         motorRight.setPower(-gamepad1.right_stick_y);
         motorLeft.setPower(gamepad1.left_stick_y);
-
-        if(i % 25 == 0) {
-            //Intake Lift Servo
-            if (gamepad1.right_bumper) {
-                intakeLiftServoRight.setPosition(1);
-                intakeLiftServoLeft.setPosition(0);
-            }
-            else if (gamepad1.left_bumper) {
-                intakeLiftServoRight.setPosition(0);
-                intakeLiftServoLeft.setPosition(1);
-            }
-            else {
-                intakeLiftServoRight.setPosition(0.5);
-                intakeLiftServoLeft.setPosition(0.5);
-            }
-        }
 
         //Spinning Collector Motor
         //Set power to negative to make the things go in, positive to make things go out.
